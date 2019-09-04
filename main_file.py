bce226b2ae3e326b3ba155f284d4c04093d3b26f
Lahiru Dilshan <dilshankmg@gmail.com>
2019-09-04 09:52:08 +0530

    Change parameters

--- main_file.py
+++ main_file.py
@@ -1,22 +1,43 @@
 import function_file as ff
 import sympy as sp
-import numpy as np
-from scipy.optimize import newton
-from scipy.optimize import fsolve
+import time
 
+start = time.time()
+
+# ------ airfoil data
 airfoil = 'NACA4412'
 N, r, center_circle, trailing_edge_z, Gkn, z_plane, v_plane, u_plane = ff.read_data(airfoil)
+# ------ free stream velocity
+free_velocity = 20.0
+free_aoa = 2.0
+free_aoa = sp.rad(free_aoa).evalf()
+# ------ plunging parameters
+pl_amplitude = 2
+pl_frequency = 3
+# ------ time step
+time_step = 0.02
+current_time = 0.00
+iteration = 50
+# ------ new vortex
+distance = 0.05
+angle = 0
+# ------ data store
+circulation = []
+le_vortex_strength = []
+vortex_z = []
+vortex_v = []
+vortex_u = []
+# derivatives
 
 v_func, v_derive = ff.get_v_function(Gkn, r, center_circle)
-tolerance = 1e-10
-iteration = 50
 
-z = sp.symbols('z', real=False)
 func = v_func - trailing_edge_z
 derive_func = v_derive
+'''
+tolerance = 1e-8
+iteration = 50
 v_value = ff.newton(func, derive_func, center_circle + r, tolerance, iteration)
 u_value = ff.get_u_value(v_value, center_circle, r)
-print(v_value)
-print(u_value)
+'''
 
 ####
